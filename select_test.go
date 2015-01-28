721a8b6527c0abb0fd9b48855f5a8c12f61d58a4
umisama <umisama@fe2o3.jp>
2015-01-28 16:10:13 +0900

    Added benchmark for SELECT statement
    benchmark on my X1 carbon
    $ go test -bench="." -benchmem=true
    PASS
    BenchmarkSelect	200000	6464 ns/op	1408 B/op	46 allocs/op
    ok      github.com/umisama/go-sqlbuilder        1.366s

--- select_test.go
+++ select_test.go
@@ -31,3 +31,23 @@ func TestSelect(t *testing.T) {
 		t.Error("got", err)
 	}
 }
+
+func BenchmarkSelect(b *testing.B) {
+	table1, _ := NewTable(
+		"TABLE_A",
+		IntColumn("id", false),
+		IntColumn("test1", false),
+		IntColumn("test2", false),
+	)
+
+	for i := 0; i < b.N; i++ {
+		Select(table1.C("test1"), table1.C("test2")).
+			From(table1).
+			Where(
+			And(
+				Eq(table1.C("id"), L(1)),
+				Eq(table1.C("test1"), L(2)),
+			)).
+			ToSql()
+	}
+}
