51db22aeaff3c5195814d380b079af7005bd9564
umisama <umisama@fe2o3.jp>
2015-02-03 11:27:10 +0900

    Added some methods to Update statement

--- update.go
+++ update.go
@@ -1,9 +1,15 @@
 package sqlbuilder
 
+import "strconv"
+
 type UpdateStatement struct {
-	table Table
-	set   []serializable
-	where Condition
+	table      Table
+	set        []serializable
+	where      Condition
+	orderByAsc string
+	orderBy    []serializable
+	limit      int
+	offset     int
 }
 
 func Update(table Table) *UpdateStatement {
@@ -23,18 +29,63 @@ func (b *UpdateStatement) Where(cond Condition) *UpdateStatement {
 	return b
 }
 
+func (b *UpdateStatement) Limit(limit int) *UpdateStatement {
+	b.limit = limit
+	return b
+}
+
+func (b *UpdateStatement) Offset(offset int) *UpdateStatement {
+	b.offset = offset
+	return b
+}
+
+func (b *UpdateStatement) OrderBy(desc bool, columns ...Column) *UpdateStatement {
+	ex_column := make([]serializable, len(columns))
+	for i := range columns {
+		ex_column[i] = columns[i]
+	}
+	b.orderBy = ex_column
+
+	if desc {
+		b.orderByAsc = " DESC"
+	} else {
+		b.orderByAsc = " ASC"
+	}
+	return b
+}
+
 func (b *UpdateStatement) ToSql() (string, []interface{}, error) {
 	bldr := newBuilder()
 
+	// UPDATE TABLE SET (COLUMN=VALUE)
 	bldr.Append("UPDATE ")
 	bldr.AppendItem(b.table)
-
 	bldr.Append(" SET ")
 	bldr.AppendItems(b.set, ", ")
 
+	// WHERE
 	if b.where != nil {
 		bldr.Append(" WHERE ")
 		bldr.AppendItem(b.where)
+	}
+
+	// ORDER BY
+	if b.orderBy != nil {
+		bldr.Append(" ORDER BY ")
+		bldr.AppendItems(b.orderBy, ",")
+		bldr.Append(b.orderByAsc)
+	}
+
+	// LIMIT
+	if b.limit != 0 {
+		bldr.Append(" LIMIT ")
+		bldr.Append(strconv.Itoa(b.limit))
+	}
+
+	// Offset
+	if b.offset != 0 {
+		bldr.Append(" OFFSET ")
+		bldr.Append(strconv.Itoa(b.offset))
 	}
 
 	bldr.Append(dialect.QuerySuffix())
--- update_test.go
+++ update_test.go
@@ -18,9 +18,11 @@ func TestUpdate(t *testing.T) {
 		Set(table1.C("test1"), 10).
 		Set(table1.C("test2"), 20).
 		Where(table1.C("id").Eq(1)).
+		OrderBy(true, table1.C("test1")).
+		Limit(1).
+		Offset(2).
 		ToSql()
-	a.Equal(`UPDATE "TABLE_A" SET "test1"=?, "test2"=? WHERE "TABLE_A"."id"=?;`, query)
+	a.Equal(`UPDATE "TABLE_A" SET "test1"=?, "test2"=? WHERE "TABLE_A"."id"=? ORDER BY "TABLE_A"."test1" DESC LIMIT 1 OFFSET 2;`, query)
 	a.Equal([]interface{}{int64(10), int64(20), int64(1)}, attrs)
 	a.Nil(err)
-
 }
