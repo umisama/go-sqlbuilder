bdc6fc5e0a11c525d8532aba5b2b77afe10cfffa
umisama <umisama@fe2o3.jp>
2015-02-09 18:29:28 +0900

    Better integration test

--- integration_test/main_test.go
+++ integration_test/main_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
@@ -12,8 +13,83 @@ import (
 	_ "github.com/ziutek/mymysql/godrv"
 )
 
-var table1 sb.Table
 var db *sql.DB
+
+// Table for testing
+var tbl_person = sb.NewTable(
+	"PERSON",
+	sb.IntColumn("id", sb.CO_PrimaryKey),
+	sb.StringColumn("name", 255, sb.CO_Unique),
+	sb.DateColumn("birth"),
+)
+var tbl_phone = sb.NewTable(
+	"PHONE",
+	sb.IntColumn("id", sb.CO_PrimaryKey, sb.CO_AutoIncrement),
+	sb.IntColumn("person_id"),
+	sb.StringColumn("number", 255),
+)
+var tbl_email = sb.NewTable(
+	"EMAIL",
+	sb.IntColumn("id", sb.CO_PrimaryKey, sb.CO_AutoIncrement),
+	sb.IntColumn("person_id"),
+	sb.StringColumn("address", 255),
+)
+
+// Data for testing
+type Person struct {
+	Id    int
+	Name  string
+	Birth time.Time
+}
+
+type Phone struct {
+	PersonId int
+	Number   string
+}
+
+type Email struct {
+	PersonId int
+	Address  string
+}
+
+var persons = []Person{{
+	Id:    1,
+	Name:  "Rintaro Okabe",
+	Birth: time.Date(1991, time.December, 14, 0, 0, 0, 0, time.UTC),
+}, {
+	Id:    2,
+	Name:  "Mayuri Shiina",
+	Birth: time.Date(1994, time.February, 1, 0, 0, 0, 0, time.UTC),
+}, {
+	Id:    3,
+	Name:  "Itaru Hashida",
+	Birth: time.Date(1991, time.May, 19, 0, 0, 0, 0, time.UTC),
+}}
+
+var phones = []Phone{{
+	PersonId: 1,
+	Number:   "000-0000-0000",
+}, {
+	PersonId: 2,
+	Number:   "111-1111-1111",
+}, {
+	PersonId: 2,
+	Number:   "111-1111-2222",
+}}
+
+var emails = []Email{{
+	PersonId: 1,
+	Address:  "sg-epk@jtk93.x29.jp",
+}, {
+	PersonId: 1,
+	Address:  "okarin@example.org",
+}, {
+	PersonId: 2,
+	Address:  "mayusii@example.org",
+}, {
+	PersonId: 3,
+	Address:  "hashida@example.org",
+}}
 
 func TestMain(m *testing.M) {
 	results := make(map[string]int)
@@ -23,18 +99,12 @@ func TestMain(m *testing.M) {
 		driver  string
 		dsn     string
 	}
+
 	var cases = []testcase{
 		{"sqlite", sb.SqliteDialect{}, "sqlite3", ":memory:"},
 		{"mymysql", sb.MysqlDialect{}, "mymysql", "go_sqlbuilder_test/root/"},
 		{"postgres", sb.PostgresDialect{}, "postgres", "user=postgres dbname=go_sqlbuilder_test sslmode=disable"},
 	}
-
-	table1 = sb.NewTable(
-		"TABLE_A",
-		sb.IntColumn("id", sb.CO_PrimaryKey),
-		sb.IntColumn("value1"),
-		sb.StringColumn("value2", 255),
-	)
 
 	for _, c := range cases {
 		fmt.Println("START unit test for", c.name)
--- integration_test/scenario_test.go
+++ integration_test/scenario_test.go
@@ -2,6 +2,7 @@ package sqlbuilder_integration
 
 import (
 	"testing"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
@@ -12,81 +13,172 @@ import (
 func TestCreateTable(t *testing.T) {
 	a := assert.New(t)
 
-	query, args, err := sb.CreateTable(table1).ToSql()
+	for _, table := range []sb.Table{tbl_person, tbl_phone, tbl_email} {
+		query, args, err := sb.CreateTable(table).ToSql()
+		a.NoError(err)
+
+		_, err = db.Exec(query, args...)
+		a.NoError(err)
+	}
+}
+
+func TestCreateIndex(t *testing.T) {
+	a := assert.New(t)
+	for _, table := range []sb.Table{tbl_phone, tbl_email} {
+		query, args, err := sb.CreateIndex(table).Name("I_" + table.Name() + "_PERSONID").Columns(table.C("person_id")).
+			IfNotExists().
+			ToSql()
+		a.NoError(err)
+
+		_, err = db.Exec(query, args...)
+		a.NoError(err)
+	}
+}
+
+func TestInsertToPersonTable(t *testing.T) {
+	a := assert.New(t)
+	for _, person := range persons {
+		query, args, err := sb.Insert(tbl_person).
+			Columns(tbl_person.C("id"), tbl_person.C("name"), tbl_person.C("birth")).
+			Values(person.Id, person.Name, person.Birth).
+			ToSql()
+		a.NoError(err)
+
+		_, err = db.Exec(query, args...)
+		a.NoError(err)
+	}
+}
+
+func TestInsertToPhoneTable(t *testing.T) {
+	a := assert.New(t)
+	for _, phone := range phones {
+		query, args, err := sb.Insert(tbl_phone).
+			Columns(tbl_phone.C("person_id"), tbl_phone.C("number")).
+			Values(phone.PersonId, phone.Number).
+			ToSql()
+		a.NoError(err)
+
+		_, err = db.Exec(query, args...)
+		a.NoError(err)
+	}
+}
+
+func TestInsertToEmailTable(t *testing.T) {
+	a := assert.New(t)
+	for _, email := range emails {
+		query, args, err := sb.Insert(tbl_email).
+			Columns(tbl_email.C("person_id"), tbl_email.C("address")).
+			Values(email.PersonId, email.Address).
+			ToSql()
+		a.NoError(err)
+
+		_, err = db.Exec(query, args...)
+		a.NoError(err)
+	}
+}
+
+func TestSelectSimple(t *testing.T) {
+	a := assert.New(t)
+
+	query, args, err := sb.Select(
+		tbl_person.C("id"), tbl_person.C("name"), tbl_person.C("birth")).
+		From(tbl_person).
+		OrderBy(false, tbl_person.C("id")).
+		ToSql()
+	a.NoError(err)
+	rows, err := db.Query(query, args...)
+	a.NoError(err)
+
+	got_persons := make([]Person, 0)
+	if a.NotNil(rows) {
+		for rows.Next() {
+			id, name, birth := 0, "", time.Time{}
+			err := rows.Scan(&id, &name, &birth)
+			a.NoError(err)
+			got_persons = append(got_persons, Person{
+				Id:    id,
+				Name:  name,
+				Birth: birth.UTC(),
+			})
+		}
+		rows.Close()
+	}
+	a.Equal(got_persons, persons)
+}
+
+func TestSelectJoined(t *testing.T) {
+	a := assert.New(t)
+	type PersonEmail struct {
+		Id    int
+		Name  string
+		Birth time.Time
+		Email string
+	}
+	expect := []PersonEmail{{
+		Id:    1,
+		Name:  "Rintaro Okabe",
+		Birth: time.Date(1991, time.December, 14, 0, 0, 0, 0, time.UTC),
+		Email: "sg-epk@jtk93.x29.jp",
+	}, {
+		Id:    1,
+		Name:  "Rintaro Okabe",
+		Birth: time.Date(1991, time.December, 14, 0, 0, 0, 0, time.UTC),
+		Email: "okarin@example.org",
+	}, {
+		Id:    2,
+		Name:  "Mayuri Shiina",
+		Birth: time.Date(1994, time.February, 1, 0, 0, 0, 0, time.UTC),
+		Email: "mayusii@example.org",
+	}, {
+		Id:    3,
+		Name:  "Itaru Hashida",
+		Birth: time.Date(1991, time.May, 19, 0, 0, 0, 0, time.UTC),
+		Email: "hashida@example.org",
+	}}
+
+	tbl_person_email := tbl_person.LeftOuterJoin(tbl_email, tbl_email.C("person_id").Eq(tbl_person.C("id")))
+	query, args, err := sb.Select(
+		tbl_person.C("id"), tbl_person.C("name"), tbl_person.C("birth"), tbl_email.C("address")).
+		From(tbl_person_email).
+		OrderBy(false, tbl_person.C("id")).
+		OrderBy(false, tbl_email.C("id")).
+		ToSql()
+	a.NoError(err)
+	rows, err := db.Query(query, args...)
+	a.NoError(err)
+
+	got_persons := make([]PersonEmail, 0)
+	if a.NotNil(rows) {
+		for rows.Next() {
+			id, name, birth, email := 0, "", time.Time{}, ""
+			err := rows.Scan(&id, &name, &birth, &email)
+			a.NoError(err)
+			got_persons = append(got_persons, PersonEmail{
+				Id:    id,
+				Name:  name,
+				Birth: birth.UTC(),
+				Email: email,
+			})
+		}
+		rows.Close()
+	}
+	a.Equal(expect, got_persons)
+}
+
+func TestDelete(t *testing.T) {
+	a := assert.New(t)
+	query, args, err := sb.Delete(tbl_phone).Where(tbl_phone.C("id").Eq(1)).ToSql()
 	a.NoError(err)
 
 	_, err = db.Exec(query, args...)
 	a.NoError(err)
 }
 
-func TestInsert(t *testing.T) {
+func TestSqlFunction1(t *testing.T) {
 	a := assert.New(t)
-
-	// data 1
-	query, args, err := sb.Insert(table1).
-		Columns(table1.C("id"), table1.C("value1"), table1.C("value2")).
-		Values(1, 10, "teststring").
-		ToSql()
-	a.NoError(err)
-
-	result, err := db.Exec(query, args...)
-	a.NoError(err)
-
-	if a.NotNil(result) {
-		rows_affected, err := result.RowsAffected()
-		a.Equal(1, rows_affected)
-		a.NoError(err)
-	}
-
-	// data 2
-	query, args, err = sb.Insert(table1).
-		Columns(table1.C("id"), table1.C("value1"), table1.C("value2")).
-		Values(2, 20, "teststring").
-		ToSql()
-	a.NoError(err)
-
-	result, err = db.Exec(query, args...)
-	a.NoError(err)
-
-	if a.NotNil(result) {
-		rows_affected, err := result.RowsAffected()
-		a.Equal(1, rows_affected)
-		a.NoError(err)
-	}
-}
-
-func TestSelect(t *testing.T) {
-	a := assert.New(t)
-	query, args, err := sb.Select(table1.C("id"), table1.C("value1"), table1.C("value2")).
-		From(table1).
-		Where(table1.C("id").Eq(1)).
-		Limit(1).OrderBy(false, table1.C("id")).ToSql()
-	a.NoError(err)
-
-	rows, err := db.Query(query, args...)
-	a.NoError(err)
-
-	if a.NotNil(rows) {
-		defer rows.Close()
-		count := 0
-		for rows.Next() {
-			id, value1, value2 := 0, 0, ""
-			err := rows.Scan(&id, &value1, &value2)
-			a.NoError(err)
-			a.Equal(1, id)
-			a.Equal(10, value1)
-			a.Equal("teststring", value2)
-			count += 1
-		}
-		a.Equal(1, count)
-
-		rows.Close()
-	}
-}
-
-func TestSqlFunction(t *testing.T) {
-	a := assert.New(t)
-	query, args, err := sb.Select(sb.Func("count", table1.C("id"))).From(table1).ToSql()
+	query, args, err := sb.Select(
+		sb.Func("count", tbl_phone.C("id")),
+	).From(tbl_phone).ToSql()
 	a.NoError(err)
 
 	rows, err := db.Query(query, args...)
@@ -101,20 +193,13 @@ func TestSqlFunction(t *testing.T) {
 	}
 }
 
-func TestDelete(t *testing.T) {
-	a := assert.New(t)
-	query, args, err := sb.Delete(table1).Where(table1.C("id").Eq(0)).ToSql()
-	a.NoError(err)
-
-	_, err = db.Exec(query, args...)
-	a.NoError(err)
-}
-
 func TestDropTable(t *testing.T) {
 	a := assert.New(t)
-	query, args, err := sb.DropTable(table1).ToSql()
-	a.NoError(err)
+	for _, table := range []sb.Table{tbl_person, tbl_phone, tbl_email} {
+		query, args, err := sb.DropTable(table).ToSql()
+		a.NoError(err)
 
-	_, err = db.Exec(query, args...)
-	a.NoError(err)
+		_, err = db.Exec(query, args...)
+		a.NoError(err)
+	}
 }
